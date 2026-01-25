db5ec14c1fcd2a2b2a389ac2f9a6023653ce33e2
asaky <asaukiuki@gmail.com>
2026-01-25 16:28:37 +0900

    first commit

---
+++ src/app/Http/Controllers/TestController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TestController extends Controller
+{
+//    public function index($text = "デフォルト")
+    public function index(Request $request)
+    {
+        $item = [
+            'content' => 'パラメータを渡す',
+//            'param' => $text
+            'param' => $request->text
+        ];
+        return  view('index', $item);
+    }
+}
